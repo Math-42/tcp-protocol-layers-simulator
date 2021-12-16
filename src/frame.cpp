d93633176a730b1a1c6b9af67929d0729a7dcdf5
math42 <math42@pop-os.localdomain>
2021-12-16 03:18:12 -0300

    Adicionando funcoes uteis para o frame

---
+++ src/frame.cpp
@@ -0,0 +1,37 @@
+#include "frame.hpp"
+
+void printBytes(std::vector<bool> bits) {
+    std::cout << std::endl;
+    int i = 1;
+    int j = 1;
+    for (auto bit : bits) {
+        std::cout << bit;
+        i = i % 8 == 0 ? 1 : i + 1;
+        if (i == 1) std::cout << " ";
+        j = j % 32 == 0 ? 1 : j + 1;
+        if (j == 1) std::cout << std::endl;
+    }
+    std::cout << std::endl;
+}
+
+void printStream(std::vector<bool> bits) {
+    std::cout << std::endl;
+    int i = 1;
+    for (auto bit : bits) {
+        std::cout << bit;
+        i = i % 16 == 0 ? 1 : i + 1;
+        if (i == 1) std::cout << std::endl;
+    }
+    std::cout << std::endl;
+}
+
+void printDiff(std::vector<bool> original, std::vector<bool> newVersion) {
+    std::cout << std::endl;
+    for (int i = 0; i < original.size() ; i++) {
+        std::cout << ((original[i] != newVersion[i]) ? "\033[1m\033[31m" : "\033[0m\033[0m") << original[i] <<
+            "\033[0m\033[0m";
+        ;
+        if ((i+1) %16 == 0) std::cout << std::endl;
+    }
+    std::cout << std::endl;
+}
---
+++ src/frame.hpp
@@ -0,0 +1,9 @@
+#pragma once
+#include <vector>
+#include <iostream>
+
+void printBytes(std::vector<bool> bits);
+
+void printStream(std::vector<bool> bits);
+
+void printDiff(std::vector<bool> original, std::vector<bool> newVersion);
